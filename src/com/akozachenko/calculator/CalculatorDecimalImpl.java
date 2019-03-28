12beac28eaf987265a02fec2beacf6d1e54f8653
Andrew Kozachenko <andrew.kozachenko@gmail.com>
2019-03-28 22:05:40 +0200

    formate according to code style

--- src/com/akozachenko/calculator/CalculatorDecimalImpl.java
+++ src/com/akozachenko/calculator/CalculatorDecimalImpl.java
@@ -3,27 +3,27 @@ package com.akozachenko.calculator;
 public class CalculatorDecimalImpl implements Calculator {
     @Override
     public int sum(int a, int b) {
-        return a+b;
+        return a + b;
     }
 
     @Override
     public int minus(int a, int b) {
-        return a-b;
+        return a - b;
     }
 
     @Override
     public int multiplication(int a, int b) {
-        return a*b;
+        return a * b;
     }
 
     @Override
     public double divide(int a, int b) throws ArithmeticException {
-        return a/b;
+        return a / b;
     }
 
     @Override
     public double getPercent(double number, double percent) {
-        return (number/100)*percent;
+        return (number / 100) * percent;
     }
 
     @Override
